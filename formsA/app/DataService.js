c342ffb4db6097f049b476390b08575dbdc7e816
Dean <dean92@gmail.com>
2015-08-21 16:05:29 -0500

    another attempt

---
+++ formsA/app/DataService.js
@@ -0,0 +1,35 @@
+﻿
+angularFormsApp.factory('DataService',
+    function () {
+        var getEmployee = function (id) {
+            if (id == 123) {
+                return {
+                    id: 123,
+                    fullName: "Dean McCoy",
+                    notes: "The ideal employee. Just don't touch my red stapler",
+                    department: "Administration",
+                    dateHired: "March 7 2007",
+                    breakTime: "March 7 2007 9:00 AM",
+                    perkCar: true,
+                    perkStock: false,
+                    perkSixWeeks: true,
+                    payrollType: "none"
+                };
+            }
+            return undefined;
+        };
+
+        var insertEmployee = function (newEmployee) {
+            return true;
+        };
+
+        var updateEmployee = function (employee) {
+            return true;
+        };
+
+        return {
+            insertEmployee: insertEmployee,
+            updateEmployee: updateEmployee,
+            getEmployee: getEmployee
+        };
+    });
